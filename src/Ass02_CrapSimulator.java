e55af18ca1f348932cbf3d248b08f4cf0a1652a5
616732 <miller.616732@swl.k12.oh.us>
2025-10-20 08:51:22 -0400

    Initial commit

---
+++ src/Ass02_CrapSimulator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int rollOne;
+        int rollTwo;
+        boolean Valid = false;
+        System.out.println("Welcome to the Craps simulator.");
+        System.out.println("Rule 1: if the sum is 2, 3 or 12, you lose.");
+        System.out.println("Rule 2: if the sum is 7 or 11, you lose.");
+        System.out.println("Rule 3: For all other values, the sum becomes the point.");
+        while (!Valid) {
+
+        }
+    }
+}
